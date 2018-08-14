72ab118a6b13d8af7e7e1f58714a6ab6467dd666
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2018-08-13 20:06:39 -0600

    Add files via upload

--- Proyecto.java
+++ Proyecto.java
@@ -1,15 +1,19 @@
 package Datos;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
 
 public class Proyecto {
 
     private Map<String, Actividad> actividades;
+    private Actividad start = new Actividad("start", 0);
+    private Actividad end = new Actividad("end", 0);
 
     public Proyecto() {
         actividades = new HashMap();
+        actividades.put(start.getId(), start);
+        actividades.put(end.getId(), end);
     }
 
     public void agregar(Actividad a) {
@@ -27,7 +31,8 @@ public class Proyecto {
     public void agregarAnt(String y, String x) {
         Actividad a = (Actividad) actividades.get(y);
         if (a.getId() == null ? y == null : a.getId().equals(y)) {
-            actividades.get(x).agregarA(a);
+            Actividad b = (Actividad) actividades.get(x);
+            b.agregarA(a);
         }
     }
 
@@ -40,40 +45,130 @@ public class Proyecto {
 
     }
 
-    public void CPM( String _id) {
-       Actividad a=(Actividad)actividades.get(_id);
-        while(!"end".equals(a.getId())){
-        int x = a.getSucesoras().size();
-        Actividad s=(Actividad)a.getSucesoras().get(x-1);
-        if(s.getIC()<a.getTC()){
-        s.setIC(a.getDuracion()+a.getIC());
+//    public void CPM(String _id) {
+//        Actividad a = (Actividad) actividades.get(_id);
+//        while (!"end".equals(a.getId())) {
+//            int x = a.getSucesoras().size();
+//            Actividad s = (Actividad) a.getSucesoras().get(x - 1);
+//            if (s.getIC() < a.getTC()) {
+//                s.setIC(a.getDuracion() + a.getIC());
+//            }
+//            s.setTC(s.getIC() + s.getDuracion());
+//            System.out.println(a.toString());
+//            while (x != 0) {
+//                CPM(s.getId());
+//            }
+//        }
+//
+//    }
+    public void IC(String _id) {
+        /*Version que el profesor sugirió en clase es mas eficiente no hace llamadas extra*/
+        Actividad a = (Actividad) actividades.get(_id);
+        for (Actividad s:(ArrayList<Actividad>)a.getSucesoras()) {
+            
+            s.contador++;
+            if (s.getIC() < a.TC()) {
+                s.setIC(a.getDuracion() + a.getIC());
+            }
+            s.TC();
+            if (s.contador == s.getAntecesoras().size()) {
+//                 System.out.println(s.toString()+" "+s.getIC()+ " "+ s.TC());
+                    IC(s.getId()); 
+            }
         }
-        s.setTC(s.getIC()+s.getDuracion());
-        System.out.println(a.toString());
-        while(x!=0){
-            CPM(s.getId());
-        }
+        
     }
-       
     
-    }
-    public void AddStart(Actividad start){
-       for( Map.Entry<String,Actividad> entry: actividades.entrySet()){
-           if(entry.getValue().getAntecesoras().isEmpty()){
-               start.agregarS(entry.getValue());
-               entry.getValue().agregarA(start);
-               
-           }
-       }
-    }
-    public void AddEnd(Actividad end){
-          for( Map.Entry<String,Actividad> entry: actividades.entrySet()){
-           if(entry.getValue().getSucesoras().isEmpty()){
-               entry.getValue().agregarS(end);
-               end.agregarA(entry.getValue());
-               actividades.put(end.getId(), end);
-           }
-       }
+//    public void IC(String _id){
+//        Actividad a=(Actividad) actividades.get(_id);
+//        if(!a.getId().equals("end")){
+//        for (int i=0; i<a.getSucesoras().size();i++){
+//            Actividad s=(Actividad)a.getSucesoras().get(i);
+//            if (s.getIC() < a.getTC()) {
+//                s.setIC(a.getDuracion() + a.getIC());
+//            }
+//            s.setTC(s.getIC() + s.getDuracion());
+//            System.out.println(a.toString());
+//            IC(s.getId());
+    /*version anterior del algoritmo funcionaba pero hacia llamadas extra*/
+//        }
+//      }
+//    }
+
+    public void TC(String _id) {
+        Actividad s = (Actividad) actividades.get(_id);
+        for (Actividad a: (ArrayList<Actividad>) s.getAntecesoras()) {
+            a.contador1++;
+                if (s.getSucesoras().isEmpty()) {
+                    s.setIL(s.getIC());
+                    s.TL();
+
+                } 
+                    if (a.TL() < s.getIL()) {
+                        a.TL();
+                    }
+                    a.setIL(s.TL() - a.getDuracion());
+            
+                if (a.getSucesoras().size()==a.contador1) {
+//                System.out.println(a.toString());
+                    TC(a.getId());
+                }
+            }
+        }
+    
+//    public void TC(String _id) {
+//        Actividad s = (Actividad) actividades.get(_id);
+//        
+//            for (int i = s.getAntecesoras().size(); i >0 ; i--) {
+//                Actividad a = (Actividad) s.getAntecesoras().get(i-1);
+//                if (!a.getId().equals("start")) {
+//                if (s.getIL() == 0) {
+//                    a.setTL(a.getTC());
+//                    a.setIL(a.getTL() - a.getDuracion());
+//                } else{
+//                    if(a.getTL()>s.getIL()||a.getTL()==0){
+//                        a.setTL(s.getTL()-s.getDuracion());
+//                    }
+//                    a.setIL(a.getTL()-a.getDuracion());
+//                }
+//
+////                System.out.println(a.toString());
+//                TC(a.getId());
+//            }
+//        }
+//    }
+    
+
+    public void AddStart() {
+        for (Map.Entry<String, Actividad> entry : actividades.entrySet()) {
+            if (entry.getValue().getAntecesoras().isEmpty()) {
+                if (!"start".equals(entry.getKey()) && !"end".equals(entry.getKey())) {
+                    Actividad s = (Actividad) entry.getValue();
+                    start.agregarS(s);
+                    s.agregarA(start);
+                }
+            }
+        }
     }
 
+    public void AddEnd() {
+        for (Map.Entry<String, Actividad> entry : actividades.entrySet()) {
+            if (entry.getValue().getSucesoras().isEmpty()) {
+                if (!"start".equals(entry.getKey()) && !"end".equals(entry.getKey())) {
+                    Actividad s = (Actividad) entry.getValue();
+                    end.agregarA(s);
+                    s.agregarS(end);
+                }
+            }
+        }
+
+    }
+
+    public void Imprime() {
+        for (Map.Entry<String, Actividad> entry : actividades.entrySet()) {
+//            if (entry.getValue().holgura()) {
+                System.out.println(entry.getValue().toString());
+//            }
+        }
+    }
 }
--- ReadAndWrite.java
+++ ReadAndWrite.java
@@ -1,4 +1,3 @@
-
 package Datos;
 
 import java.io.File;
@@ -10,31 +9,54 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-
 public class ReadAndWrite {
-   private File file=null;
-   private DocumentBuilderFactory doc=null;
-   private DocumentBuilder docu=null;
-   private Document document=null;
-   public ReadAndWrite(String path) throws Exception{
-       doc=DocumentBuilderFactory.newInstance();
-       docu = doc.newDocumentBuilder();
-       file= new File(path);
-       document= (Document) docu.parse(file);
-        document.getDocumentElement().normalize(); 
-   }
-   public String read(String idn, String idn2, int i){
-    NodeList as=document.getElementsByTagName(idn);
-    Node n=as.item(i);
-    if(n.getNodeType()==Node.ELEMENT_NODE){
+
+    private File file = null;
+    private DocumentBuilderFactory doc = null;
+    private DocumentBuilder docu = null;
+    private Document document = null;
+    private NodeList as = null;
+
+    public ReadAndWrite(String path) throws Exception {
+        doc = DocumentBuilderFactory.newInstance();
+        docu = doc.newDocumentBuilder();
+        file = new File(path);
+        document = (Document) docu.parse(file);
+        document.getDocumentElement().normalize();
+        as=null;
+    }
+
+    public Actividad read(String idn1, String idn2, int i) {
+        Node n = as.item(i);
+        if (n.getNodeType() == Node.ELEMENT_NODE) {
+            Element a = (Element) n;
+            int duracion=Integer.parseInt(a.getAttribute(idn2));
+            Actividad act=new Actividad(a.getAttribute(idn1),duracion);
+            return act;
+        }
+        return null;
+    }
+
+//    public int length(String idn) {
+//        return getas(idn).getLength();
+//    }
+
+    public NodeList getas(String idn) {
+        as=document.getElementsByTagName(idn);
+        return as;
+    }
+    
+//    public void setas(String idn){
+//        as=document.getElementsByTagName(idn);
+//    }
+    
+    public String readRelacion(int i,String idn){
+        Node n=as.item(i);
+        if (n.getNodeType() == Node.ELEMENT_NODE){
         Element a=(Element)n;
-        return(a.getAttribute(idn2));
+        return(a.getAttribute(idn));
     }
        return null;
 }
-   public int length(String idn){
-   NodeList as=document.getElementsByTagName(idn);
-    return as.getLength();
-}
-
+       
 }
